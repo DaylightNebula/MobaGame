edc3f45c2cc07595f52da9e80a625b5eb2152b6d
DaylightNebula <daylightnebula1@gmail.com>
2022-02-17 11:30:05 -0700

    Updated .gitingore and added a quit button to the menu scene

--- core/src/daylightnebula/mobagamegdx/scenes/MenuScene.kt
+++ core/src/daylightnebula/mobagamegdx/scenes/MenuScene.kt
@@ -1,6 +1,7 @@
 package daylightnebula.mobagamegdx.scenes
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.ai.steer.behaviors.Alignment
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.Actor
@@ -8,6 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.badlogic.gdx.utils.Align
 import daylightnebula.mobagame.network.*
 import daylightnebula.mobagame.network.datatypes.MatchType
 import daylightnebula.mobagamegdx.MobaGame
@@ -16,25 +18,41 @@ import kotlin.system.exitProcess
 
 class MenuScene: Scene() {
 
+    // ui stuff
     lateinit var stage: Stage
-    lateinit var textButton: TextButton
     lateinit var textButtonStyle: TextButton.TextButtonStyle
     lateinit var font: BitmapFont
 
+    // buttons
+    lateinit var joinQueueButton: TextButton
+    lateinit var quitButton: TextButton
+
     override fun create() {
-        // create ui
+        // create ui backbone
         stage = Stage()
         Gdx.input.inputProcessor = stage
         font = BitmapFont()
         textButtonStyle = TextButton.TextButtonStyle()
         textButtonStyle.font = font
-        textButton = TextButton("Join Queue", textButtonStyle)
-        stage.addActor(textButton)
+
+        // create buttons
+        joinQueueButton = TextButton("Join Queue", textButtonStyle)
+        quitButton = TextButton("Quit", textButtonStyle)
+
+        // prepare ui stuff
+        stage.addActor(joinQueueButton)
+        stage.addActor(quitButton)
+        positionButtons(Gdx.graphics.width, Gdx.graphics.height)
 
         // button listeners
-        textButton.addListener(object : ChangeListener() {
+        joinQueueButton.addListener(object : ChangeListener() {
             override fun changed(event: ChangeEvent?, actor: Actor?) {
                 joinQueue(MatchType.NORMALS)
+            }
+        })
+        quitButton.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent?, actor: Actor?) {
+                exitProcess(0)
             }
         })
     }
@@ -42,7 +60,7 @@ class MenuScene: Scene() {
     override fun render() {
         // setup render
         Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
         stage.draw()
     }
@@ -51,7 +69,15 @@ class MenuScene: Scene() {
         Gdx.input.inputProcessor = null
     }
 
-    override fun resize(width: Int, height: Int) {}
+    override fun resize(width: Int, height: Int) {
+        positionButtons(width, height)
+    }
+
+    fun positionButtons(width: Int, height: Int) {
+        if (!this::joinQueueButton.isInitialized) return
+        joinQueueButton.setPosition(width / 2f, height / 2f, Align.center)
+        quitButton.setPosition(width / 2f, height / 2f - joinQueueButton.height, Align.center)
+    }
 
     override fun processServerPacket(serverPacket: ServerPacket): Boolean {
         if (serverPacket is LoginPacket) {
